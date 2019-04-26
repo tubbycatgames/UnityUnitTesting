e21e899fab555f4b8a9fc1a25b96c06a5b611979
Phil Simmons <phil@flyclops.com>
2019-04-26 19:09:54 -0400

    Begin Unity Testing Project with basic scene and structure

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var startColor = material.color;
+        var blueShift = Input.GetAxis("Vertical");
+        var redShift = Input.GetAxis("Horizontal");
+        var timeShift = Time.deltaTime;
+    }
+}
